d2723b4cf0e69c320af664322facee58c5c8b57f
cowboycrackie <112209004+cowboycrackie@users.noreply.github.com>
2022-10-28 16:43:30 +0200

    prettier

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const primaryHeader = document.querySelector(".primary-header");
-const navToggle = document.querySelector(".mobile-nav-toggle");
-const primaryNav = document.querySelector(".primary-navigation");
+const primaryHeader = document.querySelector('.primary-header');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+const primaryNav = document.querySelector('.primary-navigation');
 
-navToggle.addEventListener('click', ()=> {
-  primaryNav.hasAttribute('data-visible') 
-    ? navToggle.setAttribute('aria-expanded', false) 
-    : navToggle.setAttribute('aria-expanded', true) ;
+navToggle.addEventListener('click', () => {
+  primaryNav.hasAttribute('data-visible')
+    ? navToggle.setAttribute('aria-expanded', false)
+    : navToggle.setAttribute('aria-expanded', true);
   primaryNav.toggleAttribute('data-visible');
   primaryHeader.toggleAttribute('data-overlay');
-})
+});
 
 const slider = new A11YSlider(document.querySelector('.slider'), {
   adaptiveHeight: false,
@@ -21,6 +21,6 @@ const slider = new A11YSlider(document.querySelector('.slider'), {
     560: {
       dots: false, // dots enabled 1280px and up
       // arrows: true,
-    }
-  }
+    },
+  },
 });
